44f89fa37cd5519fbddb336fcb9bc86aed365df6
Mohammad Goldast <goldast.xps@gmail.com>
2016-08-15 13:50:46 +0430

    Add id optional

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ var express = require('express'),
 	module  = require('./module'),
 	app 	= express();
 
-app.get('/search/:Name' , function (req, res, next) {
-	module.searchIMDB(req.params.Name);
+app.use(express.static(__dirname + "/statics"));
+
+
+app.get('/', function(req, res, next) {
+	res.send('/statics/index.html');
+});
+
+app.get('/s=:Name' , function (req, res, next) {
+	module.searchIMDB(req.params.Name, function(data) {
+		console.log(data);
+	});
 	res.end();
 });
 
-app.get('/getByName/:Name', function(req, res, next) {
-	module.getByName(req.params.Name);
+app.get('/t=:Name', function(req, res, next) {
+	module.getByName(req.params.Name, function(data) {
+		console.log(data);
+	});
 	res.end();
 });
 
+app.get('/i=:Id', function(req, res, next) {
+	module.getById(req.params.Id, function(data){
+		console.log(data);
+	});
+	res.end();
+});
 
 app.listen('3000');
 console.log('app runnig');
