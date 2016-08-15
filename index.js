47355c8ea3ba45f171cccece5b393628e5f1e7f2
Mohammad Goldast <goldast.xps@gmail.com>
2016-08-15 16:29:26 +0430

    Change the place to show data

--- index.js
+++ index.js
@@ -5,29 +5,22 @@ var express = require('express'),
 app.use(express.static(__dirname + "/statics"));
 
 
-app.get('/', function(req, res, next) {
-	res.send('/statics/index.html');
-});
-
 app.get('/s=:Name' , function (req, res, next) {
 	module.searchIMDB(req.params.Name, function(data) {
-		res.send(data);
+		res.json(data);
 	});
-	res.end();
 });
 
 app.get('/t=:Name', function(req, res, next) {
 	module.getByName(req.params.Name, function(data) {
-		res.send(data);
+		res.json(data);
 	});
-	res.end();
 });
 
 app.get('/i=:Id', function(req, res, next) {
 	module.getById(req.params.Id, function(data){
-		res.send(data);
+		res.json(data);
 	});
-	res.end();
 });
 
 app.listen('3000');
